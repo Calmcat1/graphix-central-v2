1c8cbd6afd8ebebc5bed0071bd618db478b22527
Lee <lgitonga101@gmail.com>
2024-01-08 21:14:49 +0300

    modified:   src/components/CTA/CTA.jsx

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
     <div>
       <div className='maincontainer'>
         <section className="jumbotron text-center pb-5 m-extra-1 pl-4"><h1 className="display-4 main-heading-styling">GraphixCentral is the one stop place for all 
-        your design and graphics needs</h1><br></br><p className="text-styling no-display-mobile">Electric Media, Logo, cards <b>You name it!</b>, <a href="#" className="custom-links">Find Your Solution here</a></p></section>
+        your design and graphics needs</h1><br></br><p className="text-styling no-display-mobile">Electric Media, Logo, cards <b>You name it!</b>, <a href="solutions" className="custom-links">Find Your Solution here</a></p></section>
       </div>
     </div>
   )
